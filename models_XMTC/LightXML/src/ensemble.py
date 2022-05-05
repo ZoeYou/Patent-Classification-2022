a9a04b5a28decfa1a7f919d126651cdba57f9ed1
ZoeYou <zuoyouwk@gmail.com>
2022-05-05 14:28:59 +0200

    add french models

--- models_XMTC/LightXML/src/ensemble.py
+++ models_XMTC/LightXML/src/ensemble.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import pandas as pd
 from torch.utils.data import DataLoader
 from dataset import MDataset, createDataCSV
 
@@ -17,16 +18,21 @@ if __name__ == '__main__':
 
     xmc_models = []
     predicts = []
-    berts = ['bert-base', 'roberta', 'xlnet', 'mbert', 'camembert']
-
+    if "-fr-" in args.dataset or "INPI" in args.dataset:
+        berts = ['camembert', 'xlm-roberta', 'mbert']#, 'camembert-large'] #xlm-roberta-large', 
+    else:
+        berts = ['bert-base', 'roberta', 'xlnet'] 
+    
+    
     for index in range(len(berts)):
         model_name = [args.dataset, '' if berts[index] == 'bert-base' else berts[index]]
         model_name = '_'.join([i for i in model_name if i != ''])
 
         model = LightXML(n_labels=len(label_map), bert=berts[index])
 
-        print(f'models/model-model_name.bin')
+        print(f'models/model-{model_name}.bin')
         model.load_state_dict(torch.load(f'models/model-{model_name}.bin'))
+
 
         tokenizer = model.get_tokenizer()
         test_d = MDataset(df, 'test', tokenizer, label_map, 128 if args.dataset == 'amazoncat13k' and berts[index] == 'xlnent' else 512)
@@ -43,11 +49,13 @@ if __name__ == '__main__':
     acc3 = [0 for i in range(len(berts) + 1)]
     acc5 = [0 for i in range(len(berts) + 1)]
 
+
     for index, true_labels in enumerate(df.label.values):
         true_labels = set([label_map[i] for i in true_labels.split()])
 
-        logits = [torch.sigmoid(predicts[i][index]) for i in range(len(berts))]
+        logits = [torch.sigmoid(predicts[i][index]) for i in range(len(berts))] # not voting, it's calculating the sum of logits of all models
         logits.append(sum(logits))
+        print(logits)
         logits = [(-i).argsort()[:10].cpu().numpy() for i in logits]
 
         for i, logit in enumerate(logits):
@@ -55,11 +63,11 @@ if __name__ == '__main__':
             acc3[i] += len(set(logit[:3]) & true_labels)
             acc5[i] += len(set(logit[:5]) & true_labels)
 
-    for i, name in enumerate(berts + ['all']):
-        p1 = acc1[i] / total
-        p3 = acc3[i] / total / 3
-        p5 = acc5[i] / total / 5
+    with open(f'./results/{args.dataset}.out', 'w') as f:
+        for i, name in enumerate(berts + ['all']):
+            p1 = acc1[i] / total
+            p3 = acc3[i] / total / 3
+            p5 = acc5[i] / total / 5
 
-        with open('./results/{args.dataset}', 'a') as f:
             print(f'{name} {p1} {p3} {p5}', file=f)
             print(f'{name} {p1} {p3} {p5}')
