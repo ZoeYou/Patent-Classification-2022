5ffc3b0301ce65784d1cb9ef57edea6002da1e73
ZoeYou <zuoyouwk@gmail.com>
2022-11-23 11:08:34 +0100

    update

--- XML/run.sh
+++ XML/run.sh
@@ -1,1 +1,8 @@
-python ensemble.py --lightxml INPI_title_abs_2020_6 --lightxml INPI_title_desc_2020_6 --lightxml INPI_claims_2020_6 --attentionxml INPI_title_abs_2020_6 --attentionxml INPI_title_desc_2020_6 --attentionxml INPI_claims_2020_6 --weighted_average
+python ensemble.py --pred_level 4 \
+--attentionxml INPI_title_claims_2022_4 \
+--attentionxml INPI_title_abs_desc_2022_4 \
+--attentionxml INPI_title_desc_2022_4 \
+--lightxml INPI_title_abs_desc_2022_4 \
+--lightxml INPI_title_abs_desc_2022_4 \
+--lightxml INPI_title_desc_2022_4 \
+
